d64050bbe1b4c5c31f2455cec95b3e0eab52c355
Swagat <swagatswaroop@gmail.com>
2023-12-23 16:31:27 +0530

    Doing react tailwind and nodejs practice app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div classname="flex flex-row h-1/2 max-h-32 min-h-14 font-sans">
+        <div classname="">
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./frontend/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
